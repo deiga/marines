7c760e502c2176e7e63a18d03ef1f827c4b4f579
Timo Sand <timo.sand@kapsi.fi>
2010-05-11 18:24:34 +0300

    Refactored some functions, corrected indentation

--- ExampleAIModule/ExampleAIModule.h
+++ ExampleAIModule/ExampleAIModule.h
@@ -2,7 +2,6 @@
 #include <BWAPI.h>
 #include <BWTA.h>
 #include <windows.h>
-#include <vector>
 
 static bool analyzed;
 static bool analysis_just_finished;
@@ -13,23 +12,25 @@ DWORD WINAPI AnalyzeThread();
 class ExampleAIModule : public BWAPI::AIModule
 {
 public:
-  virtual void onStart();
-  virtual void onEnd(bool isWinner);
-  virtual void onFrame();
-  virtual bool onSendText(std::string text);
-  virtual void onPlayerLeft(BWAPI::Player* player);
-  virtual void onNukeDetect(BWAPI::Position target);
-  virtual void onUnitCreate(BWAPI::Unit* unit);
-  virtual void onUnitDestroy(BWAPI::Unit* unit);
-  virtual void onUnitMorph(BWAPI::Unit* unit);
-  virtual void onUnitShow(BWAPI::Unit* unit);
-  virtual void onUnitHide(BWAPI::Unit* unit);
-  virtual void onUnitRenegade(BWAPI::Unit* unit);
-  virtual BWAPI::Unit* getClosestUnit(BWAPI::Unit* unit);
-  void drawStats(); //not part of BWAPI::AIModule
-  void showPlayers();
-  void showForces();
-  bool show_visibility_data;
-  std::vector<BWAPI::Unit*> ownUnits;
-  std::map<int, BWAPI::Unit*> sightedEnemies;
+    virtual void onStart();
+    virtual void onEnd(bool isWinner);
+    virtual void onFrame();
+    virtual bool onSendText(std::string text);
+    virtual void onPlayerLeft(BWAPI::Player* player);
+    virtual void onNukeDetect(BWAPI::Position target);
+    virtual void onUnitCreate(BWAPI::Unit* unit);
+    virtual void onUnitDestroy(BWAPI::Unit* unit);
+    virtual void onUnitMorph(BWAPI::Unit* unit);
+    virtual void onUnitShow(BWAPI::Unit* unit);
+    virtual void onUnitHide(BWAPI::Unit* unit);
+    virtual void onUnitRenegade(BWAPI::Unit* unit);
+    void drawStats(); //not part of BWAPI::AIModule
+    void showPlayers();
+    void showForces();
+    bool show_visibility_data;
+
+    std::vector<BWAPI::Unit*> ownUnits;
+    std::map<int, BWAPI::Unit*> sightedEnemies;
+    virtual void printPlayers();
+    virtual BWAPI::Unit* getClosestUnit(BWAPI::Unit* unit);
 };
